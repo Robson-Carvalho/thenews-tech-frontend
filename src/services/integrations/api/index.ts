3c208b9b865374dd031012875a432304ca103afe
robson-carvalho <robsoncarvalho.tech@gmail.com>
2025-02-19 23:15:30 -0300

    feat: adiciona tela de cancelamento de inscrição

--- src/services/integrations/api/index.ts
+++ src/services/integrations/api/index.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_BASE_URL;
+const baseURL = import.meta.env.VITE_API_URL;
+console.log(baseURL);
 
 const API = axios.create({ baseURL });
 
